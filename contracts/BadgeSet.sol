c17f838acebdb7bc51f60dd26192bfc882f9cf26
brian watroba <brianwatroba@gmail.com>
2022-08-03 12:52:50 -0400

    mint, _mint, and hash functions, pre testing

--- contracts/BadgeSet.sol
+++ contracts/BadgeSet.sol
@@ -1,30 +1,70 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
 
+import "@openzeppelin/contracts/access/Ownable.sol";
 import "hardhat/console.sol";
 
 error ZeroAddressMint();
+error ExpiryPassed();
 
-contract BadgeSet {
+contract BadgeSet is Ownable {
 
-    // tokenId to mapping of owners (KYCHash)
-    mapping(uint256 => mapping(bytes32 => bool)) private _ownersByBadgeId;
+    address public kycRegistry;
+    address public factory;
 
-    // tokenID to when it expires for a KYCHash
-    mapping(uint256 => mapping(bytes32 => uint256)) private _expiry;
+    // badgeId/kycHash to owned count
+    mapping(uint256 => mapping(bytes32 => bool)) private _balances;
 
-    constructor() {
+    // badgeID/kycHash to badge expiration
+    mapping(uint256 => mapping(bytes32 => uint256)) private _expiries;
 
+    constructor(address _owner, address _kycRegistry, address _factory) {
+        kycRegistry = _kycRegistry;
+        factory = _factory;
+        if (_owner != _factory) transferOwnership(_owner);
     }
 
     function _mint(
-        bytes32 to,
-        uint256 id,
-        uint256 expiry,
-        bytes memory data
-    ) internal virtual {
-        
-
+        bytes32 kycHash,
+        uint256 badgeId,
+        uint256 expiryTimestamp
+    ) internal {
+        address operator = _msgSender();
+        _balances[badgeId][kycHash] = true;
+        if (expiryTimestamp == 0) return;
+        if (expiryTimestamp <= block.timestamp) revert ExpiryPassed();
+        _expiries[badgeId][kycHash] = expiryTimestamp;
+        // TODO: look up someone's address from KYCRegistry, put in event, otherwise put in bytes32
+        emit TransferSingle(operator, address(0), kycHash, badgeId, 1);
+        // emit event
     }
+
+    function mint(
+        bytes32 kycHash,
+        uint256 badgeId
+    ) public onlyOwner {
+        _mint(kycHash, badgeId, 0);
+    }
+
+     function mintWithExpiry(
+        bytes32 kycHash,
+        uint256 badgeId,
+        uint256 expiryTimestamp
+    ) public onlyOwner {
+        _mint(kycHash, badgeId, expiryTimestamp);
+    }
+
+    function hashKyc(
+        bytes32 firstName, 
+        bytes32 lastName, 
+        uint256 dob, 
+        uint256 phoneNumber
+    ) public pure returns (bytes32) {
+        // Input sanitation will happen on front end
+        return keccak256(abi.encodePacked(firstName, lastName, dob, phoneNumber));
+    }
+
+    // TODO: edited param3 to be bytes32, normally is address
+    event TransferSingle(address indexed operator, address indexed from, bytes32 indexed to, uint256 id, uint256 value);
    
 }
