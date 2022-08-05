d044f3af18dbd3bd6fed03e55486e8cd2c5d56b0
brian watroba <brianwatroba@gmail.com>
2022-08-05 09:48:31 -0400

    basic deploy script done

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,5 +1,7 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import * as dotenv from "dotenv";
+dotenv.config();
 
 const config: HardhatUserConfig = {
   solidity: {
--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,25 +1,19 @@
 import { Provider } from "@ethersproject/abstract-provider";
-import * as dotenv from "dotenv";
 import { ethers } from "hardhat";
+import * as dotenv from "dotenv";
 dotenv.config();
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-  const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
-
-  const lockedAmount = ethers.utils.parseEther("1");
+  const provider = ethers.getDefaultProvider();
+  const signer = new ethers.Wallet(process.env.PRIVATE_KEY ?? "", provider);
 
   const BadgeSet = await ethers.getContractFactory("BadgeSet");
-  const badgeSet = await BadgeSet.deploy(forbes.address, "https://www.soulboundapi.com/");
+  const badgeSet = await BadgeSet.deploy(signer.address, "https://www.soulboundapi.com/");
+  await badgeSet.deployed();
 
-  await lock.deployed();
-
-  console.log("Lock with 1 ETH deployed to:", lock.address);
+  console.log("Contract deployed to:", badgeSet.address);
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
 main().catch((error) => {
   console.error(error);
   process.exitCode = 1;
