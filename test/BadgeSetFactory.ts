f15d8b441dcf9bf2b5b91864c5cb02bfaf64ac29
brian watroba <brianwatroba@gmail.com>
2022-08-14 10:30:25 -0400

    factory

--- test/BadgeSetFactory.ts
+++ test/BadgeSetFactory.ts
@@ -14,8 +14,8 @@ describe("BadgeSetFactory.sol", function () {
       expect(await badgeSetFactory.owner()).to.equal(soulbound.address);
     });
   });
-  describe("Write functions", () => {
-    it("createBadgeSet(): deploys a badgeSet", async () => {
+  describe("createBadgeSet", () => {
+    it("deploys a badgeSet", async () => {
       const { badgeSetFactory, soulbound, forbes, kycRegistry, uri } = await loadFixture(
         fixtures.deploy
       );
@@ -26,6 +26,15 @@ describe("BadgeSetFactory.sol", function () {
       const badgeSet = await ethers.getContractAt("BadgeSet", badgeSetAddress);
       expect(badgeSet.address).to.be.properAddress;
       expect(await badgeSet.factory()).to.equal(badgeSetFactory.address);
+    });
+  });
+  describe("badgeSets()", () => {
+    it("returns array of badgeSets", async () => {
+      const { badgeSetFactory, badgeSet, soulbound, forbes, kycRegistry, uri } = await loadFixture(
+        fixtures.deploy
+      );
+      const badgeSets = await badgeSetFactory.badgeSets();
+      expect 
     });
   });
 });
